793d4a96103d4d167905cf30c10d9b054099a854
Xinyi <xinyiwang2013@gmail.com>
2024-08-05 03:31:00 -0700

    typo for assignments file.

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import Hello from './hello.js';
 import Lab5 from './Lab5/index.js';
+import CourseRoutes from './Kanbas/Courses/routes.js';
 
 const app = express();
 app.use(cors());
 Hello(app);
 Lab5(app);
+CourseRoutes(app);
 
 const PORT = process.env.PORT || 4000;
 
