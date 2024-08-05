e6f2c373073852146f5a570fb5d08e1d5efb1432
Xinyi <xinyiwang2013@gmail.com>
2024-08-05 11:54:27 -0700

    Define the routes in the course routes js file.

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,14 +1,30 @@
 import Database from "../Database/index.js";
 export default function CourseRoutes(app) {
+  // Create a new course
   app.post("/api/courses", (req, res) => {
-    const course = { ...req.body,
-      _id: new Date().getTime().toString() };
+    const course = { ...req.body, _id: new Date().getTime().toString() };
     Database.courses.push(course);
     res.send(course);
   });
 
+  // Get all courses
   app.get("/api/courses", (req, res) => {
     const courses = Database.courses;
     res.send(courses);
+  });
+
+  // Delete a course
+  app.delete("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    Database.courses = Database.courses.filter((c) => c._id !== id);
+    res.sendStatus(204);
+  });
+
+  // Update a course
+  app.put("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    const course = req.body;
+    Database.courses = Database.courses.map((c) => (c._id === id ? { ...c, ...course } : c));
+    res.sendStatus(204);
   });
 }
