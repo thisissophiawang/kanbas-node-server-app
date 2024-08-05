af3737a558b18b91a3ff7bd9b5dc99a254d4c287
Xinyi <xinyiwang2013@gmail.com>
2024-08-05 04:10:58 -0700

    add a route that creates a new course and adds it to the Database.

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,5 +1,12 @@
 import Database from "../Database/index.js";
 export default function CourseRoutes(app) {
+  app.post("/api/courses", (req, res) => {
+    const course = { ...req.body,
+      _id: new Date().getTime().toString() };
+    Database.courses.push(course);
+    res.send(course);
+  });
+
   app.get("/api/courses", (req, res) => {
     const courses = Database.courses;
     res.send(courses);
