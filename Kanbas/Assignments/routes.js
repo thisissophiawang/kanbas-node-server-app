0bdb6698b731105bf3d9c27be4b95c1b6f07a74a
Xinyi <xinyiwang2013@gmail.com>
2024-08-05 17:45:11 -0700

    add route file for assignement.

--- Kanbas/Assignments/routes.js
+++ Kanbas/Assignments/routes.js
@@ -1,41 +1,46 @@
-import Database from "../Database/index.js";
-export default function AssignmentRoutes(app) {
-  
-  // Create a new assignment
-  app.post("/api/assignments", (req, res) => {
-    const assignment = { ...req.body, _id: new Date().getTime().toString() };
-    Database.assignments.push(assignment);
-    res.send(assignment);
-  });
+// src/Kanbas/Assignments/routes.js
 
-  // Get all assignments
-  app.get("/api/assignments", (req, res) => {
-    const assignments = Database.assignments;
-    res.send(assignments);
-  });
+import express from 'express';
+import Database from '../Database/index.js';
 
-  // Get assignments by course id
-  app.get("/api/courses/:cid/assignments", (req, res) => {
-    const { cid } = req.params;
-    const assignments = Database.assignments.filter((a) => a.course === cid);
-    res.send(assignments);
-  });
+const router = express.Router();
 
-  // Update an assignment
-  app.put("/api/assignments/:id", (req, res) => {
-    const { id } = req.params;
-    const assignmentIndex = Database.assignments.findIndex((a) => a._id === id);
-    Database.assignments[assignmentIndex] = {
-      ...Database.assignments[assignmentIndex],
-      ...req.body,
-    };
+// Create a new assignment
+router.post('/assignments', (req, res) => {
+  const assignment = { ...req.body, _id: new Date().getTime().toString() };
+  Database.assignments.push(assignment);
+  res.status(201).send(assignment);
+});
+
+// Get all assignments
+router.get('/assignments', (req, res) => {
+  res.send(Database.assignments);
+});
+
+// Get assignments by course id
+router.get('/courses/:cid/assignments', (req, res) => {
+  const { cid } = req.params;
+  const assignments = Database.assignments.filter((a) => a.course === cid);
+  res.send(assignments);
+});
+
+// Update an assignment
+router.put('/assignments/:id', (req, res) => {
+  const { id } = req.params;
+  const assignmentIndex = Database.assignments.findIndex((a) => a._id === id);
+  if (assignmentIndex !== -1) {
+    Database.assignments[assignmentIndex] = { ...Database.assignments[assignmentIndex], ...req.body };
     res.sendStatus(204);
-  });
+  } else {
+    res.sendStatus(404);
+  }
+});
 
-  // Delete an assignment
-  app.delete("/api/assignments/:id", (req, res) => {
-    const { id } = req.params;
-    Database.assignments = Database.assignments.filter((a) => a._id !== id);
-    res.sendStatus(200);
-  });
-}
+// Delete an assignment
+router.delete('/assignments/:id', (req, res) => {
+  const { id } = req.params;
+  Database.assignments = Database.assignments.filter((a) => a._id !== id);
+  res.sendStatus(200);
+});
+
+export default router;
