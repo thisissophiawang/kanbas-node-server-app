b3b45b708a420aa23b7a00ef513f1d2d2195ad2b
Xinyi <xinyiwang2013@gmail.com>
2024-08-07 13:56:14 -0700

    implement assignment routes for Retrieving assignments.

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import Hello from './hello.js';
 import Lab5 from './Lab5/index.js';
 import CourseRoutes from './Kanbas/Courses/routes.js';
 import ModuleRoutes from './Kanbas/Modules/routes.js';
-import AssignmentRoutes from './Kanbas/Assignments/routes.js';
 
 
 const app = express();
@@ -13,7 +12,6 @@ Hello(app);
 Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
-AssignmentRoutes(app);
 
 const PORT = process.env.PORT || 4000;
 
