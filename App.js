608cc0ab90cf698b56bd465dfc0ab3649617f5c9
Xinyi <xinyiwang2013@gmail.com>
2024-08-09 19:38:00 -0700

    Implementing APIs to interact with MongoDB from a React client application

--- App.js
+++ App.js
@@ -5,21 +5,23 @@ import Lab5 from './Lab5/index.js';
 import CourseRoutes from './Kanbas/Courses/routes.js';
 import ModuleRoutes from './Kanbas/Modules/routes.js';
 import AssignementRoutes from './Kanbas/Assignments/routes.js';
-import mongoose from 'mongoose';
 import "dotenv/config";
+import UserRoutes from "./Users/routes.js";
 
-
+import mongoose from 'mongoose';
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING ||"mongodb://127.0.0.1:27017/kanbas"
 mongoose.connect(CONNECTION_STRING); 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 Hello(app);
 Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignementRoutes(app);
+UserRoutes(app);
 
 const PORT = process.env.PORT || 4000;
 
---
+++ Users/routes.js
@@ -0,0 +1,24 @@
+import * as dao from "./dao.js";
+//let currentUser = null;
+
+export default function UserRoutes(app) {
+  const createUser = async (req, res) => { };
+  const deleteUser = async (req, res) => { };
+  const findAllUsers = async (req, res) => { };
+  const findUserById = async (req, res) => { };
+  const updateUser = async (req, res) => { };
+  const signup = async (req, res) => { };
+  const signin = async (req, res) => { };
+  const signout = (req, res) => { };
+  const profile = async (req, res) => { };
+
+  app.post("/api/users", createUser);
+  app.get("/api/users", findAllUsers);
+  app.get("/api/users/:userId", findUserById);
+  app.put("/api/users/:userId", updateUser);
+  app.delete("/api/users/:userId", deleteUser);
+  app.post("/api/users/signup", signup);
+  app.post("/api/users/signin", signin);
+  app.post("/api/users/signout", signout);
+  app.post("/api/users/profile", profile);
+}
