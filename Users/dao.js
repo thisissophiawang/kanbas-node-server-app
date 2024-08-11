d2c9e3e5e718daf25e92774139e9ffa84df9a9dd
Xinyi <xinyiwang2013@gmail.com>
2024-08-11 03:16:49 -0700

    implement the createUser function to insert a new user object into the users collection

--- Users/dao.js
+++ Users/dao.js
@@ -1,10 +1,11 @@
 //Users/sophiawang/2024/summer/webdev/su2/kanbas-node-server-app/Users/dao.js
 import model from "./model.js";
-export const createUser = (user) => {} // implemented later
+export const createUser = (user) => {
+  delete user._id
+  return model.create(user);
+}
 export const findAllUsers = () => model.find();
-
 export const findUserById = (userId) => model.findById(userId);
-
 export const findUserByUsername = (username) =>  model.findOne({ username: username });
 export const findUserByCredentials = (username, password) => 
      model.findOne({ username, password });
@@ -15,8 +16,9 @@ export const findUsersByPartialName = (partialName) => {
       $or: [{ firstName: { $regex: regex } },{ lastName:  { $regex: regex } }],
     });
   };
-
 export const findUsersByRole
     = (role) => model.find({ role: role }); //find all users with the given role
 export const updateUser = (userId, user) =>  model.updateOne({ _id: userId }, { $set: user });
 export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+
+
