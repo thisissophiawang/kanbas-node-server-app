061ed00f5f142c3732ece89eddc92a2918a1f8fe
Xinyi <xinyiwang2013@gmail.com>
2024-08-10 23:41:11 -0700

    Find Users By Role for routes.js

--- Users/routes.js
+++ Users/routes.js
@@ -1,13 +1,19 @@
 import * as dao from "./dao.js";
-let currentUser = null;
+
 export default function UserRoutes(app) {
   const createUser = async (req, res) => { };
   const deleteUser = async (req, res) => { };
 
-  //const findAllUsers = async (req, res) => { };
   const findAllUsers = async (req, res) => {
+    const { role } = req.query;
+    if (role) {
+    const users = await dao.findUsersByRole(role);
+    res.json(users);
+    return;
+  }
     const users = await dao.findAllUsers();
     res.json(users);
+    //return;
   };
 
   const findUserById = async (req, res) => { 
