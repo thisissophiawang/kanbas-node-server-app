c16bd828bf6040ed06c4174d9d5e9396b19dd5fd
Xinyi <xinyiwang2013@gmail.com>
2024-08-11 21:04:46 -0700

    import the dotenv library so we can determine whether we are running in our development environment and configure session accordingly.

--- App.js
+++ App.js
@@ -9,9 +9,7 @@ import "dotenv/config";
 import UserRoutes from "./Users/routes.js";
 import session from "express-session";
 
-
-
-
+// Mongoose
 import mongoose from 'mongoose';
 
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
@@ -32,20 +30,32 @@ mongoose.connection.on('disconnected', () => {
 // Connect to MongoDB
 mongoose.connect(CONNECTION_STRING);
 
+console.log(process.env.MONGO_CONNECTION_STRING);
+console.log(process.env.NETLIFY_URL);
+console.log(process.env.REMOTE_SERVER);
+
 //importing the routes
 const app = express();
 app.use(cors({
   credentials: true,
   origin: process.env.NETLIFY_URL || "http://localhost:3000",
 })
-);const sessionOptions = {
-  secret: "any string",
+);
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kanbas",
   resave: false,
   saveUninitialized: false,
 };
-app.use(
-  session(sessionOptions)
-);
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+
 
 app.use(express.json());
 
